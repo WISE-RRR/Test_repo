cc991ece9c3e4ccbac2fd1d3f24bb13f7135c672
WISE-RRR <wise010689@gmail.com>
2022-08-25 20:06:17 +0300

    Home_work_2

---
+++ DZ2/Program.cs
@@ -0,0 +1,52 @@
+﻿// Zadacha 10
+
+// int SecontDigit()
+// {
+//     int num = new Random().Next(100, 1000);
+//     Console.WriteLine("Это наше число: " + num);
+//     int sot = num / 10;
+//     int des = sot % 10;
+//     return des;
+// }
+
+// int number = SecontDigit();
+// Console.WriteLine("Вторая цифра из нашего числа: " + number);
+
+
+// Zadacha 13
+
+// К сожалению, я не смог решить задачу с помощью функции, были мысли, однако не смог скомпоновать их в коде.
+
+// Console.Write("Введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// string numText = Convert.ToString(num);
+// if (numText.Length > 2)
+// {
+//   Console.WriteLine("Третья цифра числа: " + numText[2]);
+// }
+// else 
+// {
+//   Console.WriteLine("В числе нет третьей цифры");
+// }
+
+
+//Zadacha 15
+
+// Console.WriteLine("Введите цифру обозначающую день недели: ");
+// int weekday = Convert.ToInt32(Console.ReadLine());
+
+// void CheckingTheDayOfTheWeek (int weekday)
+// {
+//   if (weekday == 6 || weekday == 7)
+//   {
+//   Console.WriteLine("Класс! Это выходной.");
+//   }
+//   else if (weekday < 1 || weekday > 7)
+//   {
+//     Console.WriteLine("Нет такого дня недели.");
+//   }
+//   else Console.WriteLine("Скука, будний день.");
+// }
+
+// CheckingTheDayOfTheWeek(weekday);
+
